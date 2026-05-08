a30e4b7dfc52aa50b9e2d030aba735d09182a80f
ZhangSunLin <131884259+Pilocode@users.noreply.github.com>
2026-05-08 20:21:35 +0800

    Implement disjoint set union (DSU) structure

--- DS/ufset.hpp
+++ DS/ufset.hpp
@@ -1,4 +1,18 @@
-#include <bits/stdc++.h>
+//并查集（路径压缩）
+struct dsu{
+    vector<int>fa;
+    dsu(int n);
+    int find(int x);//查找x所在分量的代表元
+    void unite(int x,int y); //x连接到y上
+};
+dsu::dsu(int x){
+    this->fa.resize(x);
+    for(int i = 0;i<x;i++) fa[i] = i;
+};
+int dsu::find(int x){return x==fa[x]?x:fa[x] = find(fa[x]);}
+void dsu::unite(int x,int y){fa[find(x)] = find(y);}
+//Written by Pilocode
+/*#include <bits/stdc++.h>
 using namespace std;
 struct UfSet {
     int n;
@@ -36,3 +50,4 @@ struct UfSet {
     }
 };
 using DSU = UfSet;
+*/
